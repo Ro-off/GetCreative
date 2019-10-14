95149ed150c47a667a59cec81541d2c5e7aa5999
Romeo <53314439+Ro-off@users.noreply.github.com>
2019-10-15 00:07:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -78,3 +78,9 @@ appleclickj.onclick = function() {
   showlgj.style =
     "border-radius: 10px; top:440px; position:absolute; right:40px; top:1090px; left:10px; height:700px; background-color: #202020; box-shadow: 0 -10px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);";
 };
+
+let appleclosej = document.getElementById("closeapple");
+appleclosej.onclick = function() {
+  let showlgj = document.getElementById("showlg");
+  showlgj.style = "";
+};
